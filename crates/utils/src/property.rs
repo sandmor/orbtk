e4a82cf558e7118ed24e1285beb46b7259a7f653
sandmor <sandmor1220@gmail.com>
2020-08-19 12:02:20 -0500

    add color() method to Value

--- crates/utils/src/property.rs
+++ crates/utils/src/property.rs
@@ -1,11 +1,24 @@
+use crate::{Color, Number};
 use std::iter::Peekable;
 use std::str::Chars;
+
 // Describes a RON declared function.
 #[derive(Clone, PartialEq, Debug)]
 pub enum Property {
     Color(Color),
     Method(String, Vec<Property>),
-    Other(String)
+    Number(Number, String),
+    Other(String),
+}
+
+impl Property {
+    /// Try to convert `self` into a `Number`
+    pub fn as_number(&self) -> Option<Number> {
+        match self {
+            Property::Number(number, d) if d.is_empty() => Some(*number),
+            _ => None,
+        }
+    }
 }
 
 impl From<String> for Property {
@@ -21,6 +34,15 @@ impl From<&str> for Property {
     }
 }
 
+impl Into<Number> for Property {
+    fn into(self) -> Number {
+        match self {
+            Property::Number(num, _) => num,
+            _ => Number::default(),
+        }
+    }
+}
+
 fn parse_property(chrs: &mut Peekable<Chars>) -> Property {
     let mut text = String::new();
     let method;
@@ -29,14 +51,14 @@ fn parse_property(chrs: &mut Peekable<Chars>) -> Property {
             Some('(') => {
                 method = true;
                 break;
-            },
+            }
             Some(c) => {
                 text.push(c);
-            },
+            }
             None => {
                 method = false;
                 break;
-            },
+            }
         }
     }
     if method {
@@ -45,24 +67,37 @@ fn parse_property(chrs: &mut Peekable<Chars>) -> Property {
             match chrs.peek() {
                 Some(',') => {
                     chrs.next().unwrap();
-                },
+                }
                 None | Some(')') => {
                     let _ = chrs.next();
                     break;
-                },
+                }
                 _ => {
                     args.push(parse_property(chrs));
                 }
             }
         }
         Property::Method(text, args)
-    }
-    else {
+    } else {
         if text.starts_with('#') {
-            Property::Color(Color::from(text))
+            return Property::Color(Color::from(text));
+        } else if text.starts_with(|x: char| x.is_ascii_digit() || x == '.') {
+            if let Some(mut ofs) = text.rfind(|x: char| x.is_ascii_digit() || x == '.') {
+                ofs += 1; // Moves from before last digit to after last digit position
+                if text[..ofs]
+                    .find(|x| x == '.' || x == 'e' || x == 'E')
+                    .is_some()
+                {
+                    if let Ok(v) = lexical_core::parse(text[..ofs].as_bytes()) {
+                        return Property::Number(Number::Float(v), text[ofs..].to_owned());
+                    }
+                } else {
+                    if let Ok(v) = lexical_core::parse(text[..ofs].as_bytes()) {
+                        return Property::Number(Number::Real(v), text[ofs..].to_owned());
+                    }
+                }
+            }
         }
-        else {
-            Property::Other(text)
-        }
+        Property::Other(text)
     }
 }
